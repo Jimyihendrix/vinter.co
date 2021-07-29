52aab1a6c31345b1bed15b394c84452c6f1d6f28
jimyihendrix <jimyihendrix@gmail.com>
2021-07-29 12:46:14 +0200

    app/settings.py -> # os.path.join(BASE_DIR, 'static')  <- without Dockerfile/docker-compose.yml

--- app/settings.py
+++ app/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = '/code/static/'
 
+# os.path.join(BASE_DIR, 'static')  <- without Dockerfile/docker-compose.yml
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
