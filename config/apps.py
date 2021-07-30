27c7ad2be7cbc2a6cf1eeaf49cfd510de75d3c0e
jimyihendrix <jimyihendrix@gmail.com>
2021-07-30 12:49:12 +0200

    settings.py -> INSTALLED_APP = {'app.apps.ConfigConfig'}; DUPLICATED /config/apps.py to /app/apps.py -> changed name = 'config' by 'app'; /config/models.py -> CATEGORY & class Listings(models.Model)

--- config/apps.py
+++ config/apps.py
@@ -1,6 +1,10 @@
+# this code (from line 2 to 7), has been copied at /app/apps.py
+# was changed the name = 'config' by name = 'app'
+# and accessed from settings.py INSTALLED_APPS = {'app.apps.ConfigConfig'}
 from django.apps import AppConfig
 
 
 class ConfigConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'config'
+
--- config/models.py
+++ config/models.py
@@ -1,3 +1,19 @@
+
 from django.db import models
 
 # Create your models here.
+CATEGORY = {
+    ('DRAMA','Drama'),
+    ('ACTION','Action')
+}
+
+class Listings(models.Model):
+    class Meta:
+        verbose_name = "Listings"
+        verbose_name_plural = "Listings"
+    
+    name = models.CharField(max_length=50)
+    category = models.CharField(choices=CATEGORY, max_length=6)
+
+    def __str__(self):
+        return self.name
