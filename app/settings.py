590e6d96cb4a0e6902352b1988b26edc368b9c2f
jimyihendrix <jimyihendrix@gmail.com>
2021-07-30 17:06:22 +0200

    installed: pip install psycopg2-binary

--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'config',
-    'app.apps.ConfigConfig' # localhost/admin -> user = jimy password = mirrorUser
+    'app.apps.ConfigConfig', # localhost/admin -> user = jimy password = mirrorUser
 ]
 
 
